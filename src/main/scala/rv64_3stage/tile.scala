50fd2ef2ff4895ce9d283e9083253437e58fd621
zhxj9823 <zhxj9823@qq.com>
2020-10-05 10:24:09 +0800

    [device] edit memory mapping

--- src/main/scala/rv64_3stage/tile.scala
+++ src/main/scala/rv64_3stage/tile.scala
@@ -11,13 +11,12 @@ class TileIO extends Bundle with phvntomParams {
   // TODO
 }
 
-
-class Tile extends Module with phvntomParams with projectConfig{
+class Tile extends Module with phvntomParams with projectConfig {
   val io = IO(new TileIO)
 
   val core = Module(new Core)
   core.reset := reset
-  
+
   val icache = Module(new Uncache)
   val dcache = Module(new Uncache)
   val in_device = List(icache, dcache)
@@ -43,25 +42,25 @@ class Tile extends Module with phvntomParams with projectConfig{
   val addrSpace = List(
     // (Settings.getLong("MMIOBase"), Settings.getLong("MMIOSize")), // external devices
     (0x000100L, 0x10L), // POWEROFFF
-    (0x38000000L, 0x00010000L), // CLINT
-    // (0x3c000000L, 0x04000000L)  // PLIC
-    // (0x40600000L, 0x10L), // uart
-    (0x0L, 0x100000000L) // mem
+    (0x2000000L, 0x10000L), // CLINT
+    // (0xc000000L, 0x4000000L)  // PLIC
+    // (0x10000000L, 0x100L), // uart
+    (0x80000000L, 0x8000000L) // mem
   )
   val out_device = List(poweroff, clint, mem)
 
   val xbar = Module(new AXI4Xbar(2, addrSpace))
 
-  // icache.io.out <> xbar.io.in(0)  
+  // icache.io.out <> xbar.io.in(0)
   // dcache.io.out <> xbar.io.in(1)
-  for(i <- 0 until in_device.length) {
+  for (i <- 0 until in_device.length) {
     in_device(i).io.out <> xbar.io.in(i)
   }
 
   // poweroff.io.in <> xbar.io.out(0)
   // clint.io.in <> xbar.io.out(1)
-  // mem.io.in <> xbar.io.out(addrSpace.length - 1)  
-  for(i <- 0 until out_device.length) {
+  // mem.io.in <> xbar.io.out(addrSpace.length - 1)
+  for (i <- 0 until out_device.length) {
     out_device(i).io.in <> xbar.io.out(i)
   }
 }
